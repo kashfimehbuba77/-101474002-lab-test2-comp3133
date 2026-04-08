4cf09b5aaf2256c8602ce5775d98fd0f69ccef68
Kashfi <kashfimehbuba71@gmail.com>
2026-04-08 11:33:04 -0400

    fetching the character list

--- src/app/characterlist/characterlist.ts
+++ src/app/characterlist/characterlist.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { CharacterService } from '../services/character';
+import { Character } from '../models/character.model';
 
 @Component({
   selector: 'app-characterlist',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './characterlist.html',
-  styleUrl: './characterlist.css',
+  styleUrl: './characterlist.css'
 })
-export class Characterlist {}
+export class Characterlist implements OnInit {
+  characters: Character[] = [];
+
+  constructor(
+    private characterService: CharacterService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.characterService.getAllCharacters().subscribe(data => {
+      this.characters = data.filter(char => char.image);
+    });
+  }
+
+  goToDetails(id: string): void {
+    this.router.navigate(['/character', id]);
+  }
+}
