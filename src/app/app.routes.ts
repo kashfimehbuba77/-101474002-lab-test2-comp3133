4cfddc7ae2ffaeb70e99d2156630a7f98155a7ef
Kashfi <kashfimehbuba71@gmail.com>
2026-04-08 11:29:07 -0400

    Routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Characterlist } from './characterlist/characterlist';
+import { Characterfilter } from './characterfilter/characterfilter';
+import { Characterdetails } from './characterdetails/characterdetails';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'characters', pathMatch: 'full' },
+  { path: 'characters', component: Characterlist },
+  { path: 'filter', component: Characterfilter },
+  { path: 'character/:id', component: Characterdetails },
+];
